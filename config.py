7acadd2ddffefbe6393271f33bd9ba1b03843fc4
nekrulya <nekrulyaa@mail.ru>
2024-10-23 16:58:42 +0500

    Добавлены настройки по умолчанию для колонки добавления задач

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ load_dotenv()
 
 TOKEN=os.getenv('TOKEN')
 API_KEY=os.getenv('API_KEY')
+
+DEFAULT_COMPANY=os.getenv('DEFAULT_COMPANY')
+DEFAULT_PROJECT=os.getenv('DEFAULT_PROJECT')
+DEFAULT_BOARD=os.getenv('DEFAULT_BOARD')
+DEFAULT_COLUMN=os.getenv('DEFAULT_COLUMN')
