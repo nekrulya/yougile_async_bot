0eb3c4f408d8a8bdbb64de71031e6ed3ca392089
nekrulya <nekrulyaa@mail.ru>
2024-10-23 17:01:56 +0500

    Функции для запросов к API YouGile

---
+++ app/services/yougile_api.py
@@ -0,0 +1,25 @@
+import json
+
+import requests
+from config import API_KEY
+
+class URLS:
+    COMPANIES: str = "https://ru.yougile.com/api-v2/auth/companies"
+    PROJECTS: str = "https://ru.yougile.com/api-v2/projects"
+    BOARDS: str = "https://ru.yougile.com/api-v2/boards"
+    COLUMNS: str = "https://ru.yougile.com/api-v2/columns"
+    TASKS: str = "https://ru.yougile.com/api-v2/tasks"
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+async def set_task(title: str, description: str, column_id: str):
+    data = {
+        "title": title,
+        "description": description,
+        "columnId": column_id,
+    }
+    response = requests.post(URLS.TASKS, headers=HEADERS, data=json.dumps(data))
+    return response.json().get('id')
