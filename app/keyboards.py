9aad8121bc824f352b1ce63ae4eb44abc005020f
nekrulya <nekrulyaa@mail.ru>
2024-10-24 14:00:01 +0500

    Клавиатура для выбора темы

--- app/keyboards.py
+++ app/keyboards.py
@@ -6,6 +6,11 @@ main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="✅Отметить выполненной"), KeyboardButton(text="✍️Редактировать")]
 ], resize_keyboard=True, one_time_keyboard=True)
 
+task_topics = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Разработка семейств "), KeyboardButton(text="Проблемы в проекте")],
+], resize_keyboard=True, one_time_keyboard=True)
+
 task_adding_tools = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="✏️Отредактировать"), KeyboardButton(text="✉️Отправить задачу")],
+    [KeyboardButton(text="✏️Отредактировать описание"), KeyboardButton(text="🕐Добавить дедлайн")],
+    [KeyboardButton(text="✉️Отправить задачу")],
 ], resize_keyboard=True, one_time_keyboard=True)
