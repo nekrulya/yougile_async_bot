39b958e3d861d5abae7b782b4ffd02a4d2356330
nekrulya <nekrulyaa@mail.ru>
2024-10-25 13:41:34 +0500

    Изменено приветствие

--- app/handlers/main_handlers.py
+++ app/handlers/main_handlers.py
@@ -22,6 +22,6 @@ class TaskAdding(StatesGroup):
 async def cmd_start(message: Message, state=FSMContext):
     await rq.set_user(message.from_user.id)
     await state.clear()
-    await message.answer(f"hello {message.from_user.first_name}", reply_markup=kb.main)
+    await message.answer(f"Приветствую, {message.from_user.first_name}!", reply_markup=kb.main)
 
 
